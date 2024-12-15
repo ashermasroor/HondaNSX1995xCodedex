ca5cf95f7ee4ad6ec2fb9ecaaf16d2763ac722c5
ashermasroor <syed.asher.masroor@gmail.com>
2024-12-16 00:50:48 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function navigateToindex() {
+    window.location.href = 'index.html';
+  }
+
+function navigateToLove() {
+    window.location.href = 'Love.html';
+  }
+
+function navigateToSpecs() {
+    window.location.href = 'Specs.html';
+  }
